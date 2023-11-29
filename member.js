536b7bafd45ae45b85d5449332847296b6215c8d
José-Gilberto <92352192+Gil987654321@users.noreply.github.com>
2023-11-29 05:05:40 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,8 @@
+function skillMenu() {
+    var skillMenu = document.getElementById("skillMenu");
+    if (skillMenu.style.display === "none") {
+        skillMenu.style.display = "block";
+    } else {
+        skillMenu.style.display = "none";
+    }
+}
